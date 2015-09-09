90a9ce887bc0be707fd8657256c592b6ffd35a05
blukat29 <yunjong@kaist.ac.kr>
2015-09-09 22:01:29 +0900

    First commit

---
+++ extract.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+tooldir=$( dirname $0 )/tools; echo $tooldir
+dname="$( dirname $1 )"; echo $dname
+extdir=$dname/$( basename $1 .apk ); echo $extdir
+
+echo "[+] Extracting resources"
+java -jar $tooldir/apktool.jar d $1 -o $extdir/unpacked
+
+echo "[+] Extracting apk archive"
+mkdir -p $extdir
+rm -rf $extdir/raw
+mkdir -p $extdir/raw
+unzip -d $extdir/raw $1
+
+echo "[+] Converting classes.dex to jar"
+$tooldir/dex2jar-2.0/d2j-dex2jar.sh $extdir/raw/classes.dex -o $extdir/classes.jar
+
+echo "[+] Extracting jar file"
+rm -rf $extdir/cls
+mkdir -p $extdir/cls
+unzip -d $extdir/cls $extdir/classes.jar
+
+echo "[+] Decompiling class files"
+rm -rf $extdir/src
+mkdir -p $extdir/src
+$tooldir/jad/jad -r -d $extdir/src -s java $extdir/cls/**/*.class
+
+echo ""
+echo "[+] Resources are in $extdir/unpacked"
+echo "[+] Decompiled classes in $extdir/src"
+
---
+++ install.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+cd $( dirname $0 )
+mkdir -p tools
+cd tools
+if [ ! -d dex2jar-2.0 ]; then
+  wget https://github.com/pxb1988/dex2jar/releases/download/2.0/dex-tools-2.0.zip -O dex2jar.zip
+  unzip dex2jar.zip
+  rm dex2jar.zip
+  chmod +x dex2jar-2.0/*.sh
+fi
+
+if [ ! -d jad ]; then
+  wget http://varaneckas.com/jad/jad158g.mac.intel.zip -O jad.zip
+  unzip -d jad jad.zip
+  rm jad.zip
+fi
+
+if [ ! -f apktool.jar ]; then
+  wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.1.jar -O apktool.jar
+fi
+
